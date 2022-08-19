ec0620ea156c6bd42a0083167f420e336ac46ad8
Alex Ramsdell <aramsdell@artandlogic.com>
2022-08-19 17:04:49 -0400

    remove set_defaults call, use dest param to indicate which subparser was called in the parsed args result.

--- mt.py
+++ mt.py
@@ -3,39 +3,46 @@
 import sys
 import argparse
 
+def build(**kwargs):
+    print('kwargs: ', kwargs)
+
+def deploy(**kwargs):
+    print('kwargs: ', kwargs)
+
+def update_db(**kwargs):
+    print('kwargs: ', kwargs)
+
+def help(**kwargs):
+    print('kwargs: ', kwargs)
+
 def parse_args():
-
-    def build(args):
-        print('build!')
-
-    def deploy(args):
-        print('deploy!')
-
-    def update_db(args):
-        print('update db!')
 
 
     parser = argparse.ArgumentParser(description='machine tone website deployment tool')
-    subparsers = parser.add_subparsers()
+    subparser = parser.add_subparsers(dest='command')
 
-    parser_build = subparsers.add_parser('build', help='generate a static version of the machine tone website')
-    parser_build.set_defaults(func=build)
+    parser_build = subparser.add_parser('build', help='generate a static version of the machine tone website')
+    #parser_build.set_defaults(func=build)
 
-    parser_deploy = subparsers.add_parser('deploy', help='deploy the static build contents of ./build to the hosting server')
-    parser_deploy.set_defaults(func=deploy)
+    parser_deploy = subparser.add_parser('deploy', help='deploy the static build contents of ./build to the hosting server')
+    #parser_deploy.set_defaults(func=deploy)
 
-    parser_update_db = subparsers.add_parser('update', help='update the database using new data in the machine tone web google sheet')
-    parser_update_db.add_argument('--clean', help='prune the google sheet of all entries marked updated after pulling new data into the local sqlite database.')
-    parser_update_db.set_defaults(func=update_db)
+    parser_update_db = subparser.add_parser('update', help='update the database using new data in the machine tone web google sheet')
+    parser_update_db.add_argument('--clean', help='prune the google sheet of all entries marked updated after pulling new data into the local sqlite database.', action='store_true') 
+    #parser_update_db.set_defaults(func=update_db)
 
     return parser.parse_args(sys.argv[1:])
 
 def run():
 
-    args = parse_args()
-
-    # TODO: doesn't call help if you just call 'mt.py'
-    args.func(args)
+    args = vars(parse_args())
+    command = args['command']
+    commands = {
+        'build': build,
+        'deploy': deploy,
+        'update': update_db
+    }
+    command = commands.get(command)
 
 
 if __name__ == '__main__':
