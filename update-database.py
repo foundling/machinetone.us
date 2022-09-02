ca2d1159514ed459e031bedbf32bb75aac457335
Alex Ramsdell <aramsdell@artandlogic.com>
2022-09-02 17:12:41 -0400

    use .env file and python-dotenv to manage config vars.

--- update-database.py
+++ update-database.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python
 
+import os
+
+from dotenv import load_dotenv
 import gspread
+
+load_dotenv()
 
 service_account = gspread.service_account(filename='service_account.json')
 
-sheet = service_account.open('Website Updates Database')
-worksheet = sheet.worksheet('Artist')
+# googlesheet name is 'Machine Tone Database'
+sheet = service_account.open_by_key(os.getenv('MT_GOOGLESHEETS_DB_ID'))
+artist_worksheet = sheet.worksheet('Artist')
+release_worksheet = sheet.worksheet('Release')
 
-print(worksheet.row_count)
-print(worksheet.col_count)
+(artist_header, *artists) = artist_worksheet.col_values(1)
+(release_header, *releases) = release_worksheet.col_values(1)
+
+print(artist_header)
+print(artists)
+
+print(release_header)
+print(releases)
 
