e26f462a10df22e79ca33db35de851bdb228a5e2
Alex Ramsdell <aramsdell@artandlogic.com>
2022-09-04 13:13:48 -0400

    pass base_url into main endpoints for anchor styling.  update about page.

--- webapp/app.py
+++ webapp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -8,26 +8,24 @@ def index():
 
 @app.route('/artists')
 def artists():
+    print(request.endpoint)
     artists = ["Al's Magic","floodreed"]
-    return render_template('artists.html', artists=artists)
+    return render_template('artists.html', artists=artists, endpoint=request.endpoint)
 
 @app.route('/catalog')
 def catalog():
     releases = ["Cosmic Takes","Chakrat EP"]
-    return render_template('catalog.html', releases=releases)
+    return render_template('catalog.html', releases=releases, endpoint=request.endpoint)
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', endpoint=request.endpoint)
 
 
 # don't freeze this
 @app.route('/admin')
 def admin():
     return render_template('admin.html')
-
-
-
 
 
 if __name__ == '__main__':
