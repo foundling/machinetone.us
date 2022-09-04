cc2a5cf979aa53520e5dde01e1440f2ad149a78d
Alex Ramsdell <aramsdell@artandlogic.com>
2022-09-04 10:05:34 -0400

    update app config for freezer.

--- freeze.py
+++ freeze.py
@@ -6,8 +6,10 @@ def freeze():
     from flask_frozen import Freezer
     from webapp.app import app
 
+    # Freezer config
     build_destination = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'build')
     app.config.update(FREEZER_DESTINATION = build_destination)
+    app.config.update(FREEZER_REMOVE_EXTRA_FILES = True)
 
     freezer = Freezer(app)
 
