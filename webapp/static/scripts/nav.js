7e44f053e4da2763d1a948dd7dbaf8281a12756d
Alex Ramsdell <aramsdell@artandlogic.com>
2022-09-04 22:09:49 -0400

    some style

---
+++ webapp/static/scripts/nav.js
@@ -0,0 +1,7 @@
+const header = document.getElementById('nav-dropdown-target')
+const nav = document.getElementById('nav-dropdown')
+
+header.addEventListener('mouseover', () => {
+    nav.classList.remove('hidden');
+    console.log('hi')
+});
