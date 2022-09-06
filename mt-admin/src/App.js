e8af7dfd0703a948a3fe6d374fcf3f1ee547f7a3
Alex Ramsdell <aramsdell@artandlogic.com>
2022-09-05 21:37:12 -0400

    stub out basic app page (wip).  remove admin flask routes and replace with a json api for local-only admin panel.

--- mt-admin/src/App.js
+++ mt-admin/src/App.js
@@ -1,31 +1,67 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 
-function ArtistSelect(props) {
+/*
+ *
+ *  add artist
+ *  update artist
+ *
+ *  add release and tracks to existing artist
+ *  add release and tracks to new artist
+ *
+ *  update release
+ *  update tracks
+ *
+ */
+function ArtistSelect({ artists }) {
     
     return (
         <div className="artist-select">
-            <select>
-                <option value="">------------</option>
-                {
-                    props.artists.map(artist => <option value={artist.id}>{artist.name}</option>)
-                }
-            </select>
+            <div>
+                <label>Select an Artist: </label>
+                <select>
+                    <option key={-1} value="">------------</option>
+                    {
+                        artists.map(artist => 
+                            <option
+                                key={artist.id}
+                                value={artist.id}>
+                            {artist.name}
+                            </option>
+                        )
+                    }
+                </select>
+            </div>
+            <div>
+                or
+            </div>
+            <div>
+                add a new one <input type="text" placeholder="new artist name"/>
+            </div>
+
         </div>
     );
 }
-
 function App() {
 
-    const artists = [{
-        name: "Al's Magic",
-        id: 1,
-    },
-    {
-        name: 'floodreed',
-        id: 2
-    }];
+    const [artists, setArtists] = useState([]); 
+
+    useEffect(() => {
+
+        async function getArtists() {
+            try {
+                const { data } = await axios.get('http://localhost:5000/api/artists');
+                return data.artists;
+            } catch(e) {
+                console.log(e);
+            }
+        }
+
+        getArtists().then(setArtists);
+      
+    }, []);
 
     return (
         <div className="App">
--- webapp/app.py
+++ webapp/app.py
@@ -4,8 +4,10 @@ import sys
 from datetime import datetime
 
 from flask import Flask, render_template, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, origins=["http://localhost:3000"])
 
 def get_db_cur():
 
@@ -88,31 +90,20 @@ def catalog_item(catalog_number):
     print(release)
     return render_template('release.html', release=release)
 
-# NOTE: make sure to not freeze admin and update stuff.
-# this is going to change, using react for front-end in local-only admin panel 
-# will possibly return some sort of json
-@app.route('/admin')
-def admin():
+
+## API FOR LOCAL-ONLY ADMIN PANEL
+
+@app.route('/api/artists', methods=['GET', 'POST'])
+def api_artists():
 
     cur = get_db_cur()
+    artists = cur.execute('select * from artist').fetchall();
 
-    artists = cur.execute('select id, name from artist').fetchall()
-    genres = cur.execute('select id, name from genre').fetchall()
-    formats = cur.execute('select id, type from release_format').fetchall()
-
-    today = datetime.strftime(datetime.now(), "%Y-%m-%d")
-
-    return render_template('admin.html', artists=artists, genres=genres, formats=formats, today=today)
-
-@app.route('/update', methods=['POST'])
-def update():
-
-    d = request.form.to_dict(flat=False)
-
-    for k,v in d.items():
-        print(f"{k}: {','.join(v)}")
-
-    return render_template('update.html')
+    return {
+        "artists": [
+            dict(artist) for artist in artists
+        ]
+    }
 
 if __name__ == '__main__':
 
